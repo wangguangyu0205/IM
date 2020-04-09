80b9ab566da4222ff5a2d6d11d80d6bf5ec24dd1
wangguangyu0205 <w15721475337@163.com>
2020-04-09 18:11:55 +0800

    群验证器

--- app/Validator/GroupValidator.php
+++ app/Validator/GroupValidator.php
@@ -6,6 +6,12 @@
 
 namespace App\Validator;
 
+use Swoft\Validator\Annotation\Mapping\Enum;
+use Swoft\Validator\Annotation\Mapping\IsInt;
+use Swoft\Validator\Annotation\Mapping\IsString;
+use Swoft\Validator\Annotation\Mapping\Length;
+use Swoft\Validator\Annotation\Mapping\NotEmpty;
+use Swoft\Validator\Annotation\Mapping\Required;
 use Swoft\Validator\Annotation\Mapping\Validator;
 
 /**
@@ -15,5 +21,55 @@ use Swoft\Validator\Annotation\Mapping\Validator;
  */
 class GroupValidator
 {
+    /**
+     * @IsInt()
+     * @Required()
+     * @NotEmpty()
+     * @var int
+     */
+    protected $user_id = '';
 
+    /**
+     * @IsString()
+     * @Required()
+     * @NotEmpty()
+     * @Length(max=30)
+     * @var string
+     */
+    protected $group_name = '';
+
+    /**
+     * @IsString()
+     * @Required()
+     * @NotEmpty()
+     * @Length(max=30)
+     * @var string
+     */
+    protected $avatar = '';
+
+    /**
+     * @IsString()
+     * @Required()
+     * @NotEmpty()
+     * @Enum(values={"200","500","1000"})
+     * @var string
+     */
+    protected $size = '';
+
+    /**
+     * @IsString()
+     * @Required()
+     * @NotEmpty()
+     * @var string
+     */
+    protected  $introduction = '';
+
+    /**
+     * @IsInt()
+     * @Required()
+     * @NotEmpty()
+     * @Enum(values={"0","1"})
+     * @var int
+     */
+    protected $validation = '';
 }
