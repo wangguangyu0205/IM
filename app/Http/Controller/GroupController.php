273295bc301e37389a6810b26dd10451e99998ef
wangguangyu0205 <w15721475337@163.com>
2020-04-09 19:53:58 +0800

    添加群聊

--- app/Http/Controller/GroupController.php
+++ app/Http/Controller/GroupController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controller;
 
 use App\Model\Logic\GroupLogic;
 use Swoft\Bean\Annotation\Mapping\Inject;
+use Swoft\Http\Message\Request;
 use Swoft\Http\Server\Annotation\Mapping\Controller;
 
 /**
@@ -19,4 +20,25 @@ class GroupController
      * @var GroupLogic
      */
     protected $groupLogic;
+
+    /**
+     * @RequestMapping(route="createGroup",method={RequestMethod::POST})
+     * @Middleware(AuthMiddleware::class)
+     * @Validate(validator="GroupValidator",fields={"user_id","group_name","avatar","size","introduction","validation"})
+     */
+    public function createGroup(Request $request)
+    {
+        try {
+            $groupName = $request->parsedBody('group_name');
+            $avatar = $request->parsedBody('avatar');
+            $size = $request->parsedBody('size');
+            $introduction = $request->parsedBody('introduction');
+            $validation = $request->parsedBody('validation');
+
+            $result = $this->groupLogic->createGroup($request->user,$groupName,$avatar,$size,$introduction,$validation);
+            return apiSuccess($result);
+        }catch (\Throwable $throwable){
+            return apiError($throwable->getCode(),$throwable->getMessage());
+        }
+    }
 }
--- app/Model/Logic/GroupLogic.php
+++ app/Model/Logic/GroupLogic.php
@@ -6,6 +6,7 @@
 
 namespace App\Model\Logic;
 
+use App\ExceptionCode\ApiCode;
 use App\Model\Dao\GroupDao;
 use Swoft\Bean\Annotation\Mapping\Bean;
 use Swoft\Bean\Annotation\Mapping\Inject;
@@ -22,4 +23,25 @@ class GroupLogic
      * @var GroupDao
      */
     protected $groupDao;
+
+    public function createGroup(int $userId,string $groupName,string $avatar,string $size,string $introduction,int $validation)
+    {
+        $groupId = $this->groupDao->create([
+            'user_id' => $userId,
+            'group_name'=>$groupName,
+            'avatar'=>$avatar,
+            'size'=>$size,
+            'introduction'=>$introduction,
+            'validation'=>$validation
+        ]);
+        if (!$groupId) throw new \Exception('', ApiCode::GROUP_CREATE_FAIL);
+        $result = $this->findGroupById($groupId);
+        if (!$result) throw new \Exception('', ApiCode::GROUP_NOT_FOUND);
+        return $result;
+    }
+
+    public function findGroupById(int $groupId)
+    {
+        return $this->groupDao->findGroupById($groupId);
+    }
 }
